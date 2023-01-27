d0e0631eb2868404aa5d8b4be1e4f420d49fa5ba
Nate Appleby <npa1508@rit.edu>
2023-01-27 11:35:17 -0500

    Added all if statements and helper functions. Error Handling to be done. Expected errors at end of line.

--- JottTokenizer.java
+++ JottTokenizer.java
@@ -6,7 +6,10 @@ package provided;
  * @author 
  **/
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
 public class JottTokenizer {
 
@@ -17,6 +20,170 @@ public class JottTokenizer {
      * @return an ArrayList of Jott Tokens
      */
     public static ArrayList<Token> tokenize(String filename){
-		return null;
+		ArrayList<Token> outList = new ArrayList<>();
+		int lineNum = 0;
+		File file = new File(filename);
+		try {
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				String next_line = scan.nextLine();
+				lineNum++;
+				for (int i = 0; i < next_line.length(); i++) {
+					char next_char = next_line.charAt(i);
+					if (next_char == '#')
+					{
+						break;
+					} else if (next_char == ',')
+					{
+						Token newToken = new Token(",", filename, lineNum, TokenType.COMMA);
+						outList.add(newToken);
+					} else if (next_char == ']') {
+						Token newToken = new Token("]", filename, lineNum, TokenType.R_BRACKET);
+						outList.add(newToken);
+					} else if (next_char == '[') {
+						Token newToken = new Token("[", filename, lineNum, TokenType.L_BRACKET);
+						outList.add(newToken);
+					} else if (next_char == '}') {
+						Token newToken = new Token("}", filename, lineNum, TokenType.R_BRACE);
+						outList.add(newToken);
+					} else if (next_char == '{') {
+						Token newToken = new Token("{", filename, lineNum, TokenType.L_BRACE);
+						outList.add(newToken);
+					} else if (next_char == '=') {
+						char look_ahead = next_line.charAt(i + 1);
+						Token newToken;
+						if (look_ahead == '=') {
+							newToken = new Token("==", filename, lineNum, TokenType.REL_OP);
+						} else {
+							newToken = new Token("=", filename, lineNum, TokenType.ASSIGN);
+						}
+						outList.add(newToken);
+					} else if (next_char == '<') {
+						char look_ahead = next_line.charAt(i + 1);
+						Token newToken;
+						if (look_ahead == '=') {
+							newToken = new Token("<=", filename, lineNum, TokenType.REL_OP);
+						} else {
+							newToken = new Token("<", filename, lineNum, TokenType.REL_OP);
+						}
+						outList.add(newToken);
+					} else if (next_char == '>') {
+						char look_ahead = next_line.charAt(i + 1);
+						Token newToken;
+						if (look_ahead == '=') {
+							newToken = new Token(">=", filename, lineNum, TokenType.REL_OP);
+						} else {
+							newToken = new Token(">", filename, lineNum, TokenType.REL_OP);
+						}
+						outList.add(newToken);
+					} else if (next_char == '*' || next_char == '/' || next_char == '+' || next_char == '-') {
+						Token newToken = new Token("" + next_char, filename, lineNum, TokenType.MATH_OP);
+						outList.add(newToken);
+					} else if (next_char == ';') {
+						Token newToken = new Token(";", filename, lineNum, TokenType.SEMICOLON);
+						outList.add(newToken);
+					} else if (next_char == '.') {
+						int j = i + 1;
+						char look_ahead = next_line.charAt(j);
+						String newTok = ".";
+						if (!isDigit(look_ahead)) {
+							// TODO: Error Handling
+						}
+						while (isDigit(look_ahead)) {
+							newTok += look_ahead;
+							j++;
+							look_ahead = next_line.charAt(j);
+						}
+						Token newToken = new Token(newTok, filename, lineNum, TokenType.NUMBER);
+						outList.add(newToken);
+					} else if (isDigit(next_char)) {
+						String newTok = "" + next_char;
+						int j = i + 1;
+						char look_ahead = next_line.charAt(j);
+						while (isDigit(look_ahead)) {
+							newTok += look_ahead;
+							j++;
+							look_ahead = next_line.charAt(j);
+						}
+						if (look_ahead == '.') {
+							newTok += ".";
+							if (!isDigit(look_ahead)) {
+								// TODO: Error Handling
+							}
+							while (isDigit(look_ahead)) {
+								newTok += look_ahead;
+								j++;
+								look_ahead = next_line.charAt(j);
+							}
+						}
+						Token newToken = new Token(newTok, filename, lineNum, TokenType.NUMBER);
+						outList.add(newToken);
+					} else if (isLetter(next_char))
+					{
+						String newTok = "" + next_char;
+						int j = i + 1;
+						char look_ahead = next_line.charAt(j);
+						while(isLetter(look_ahead) || isDigit(look_ahead)) {
+							newTok += look_ahead;
+							j++;
+							look_ahead = next_line.charAt(j);
+						}
+						Token newToken = new Token(newTok, filename, lineNum, TokenType.ID_KEYWORD);
+						outList.add(newToken);
+					}
+					else if (next_char == ':') {
+						Token newToken = new Token(":", filename, lineNum, TokenType.COLON);
+						outList.add(newToken);
+					} else if (next_char == '!') {
+						if (next_line.charAt(i + 1) == '=') {
+							Token newToken = new Token("!=", filename, lineNum, TokenType.REL_OP);
+							outList.add(newToken);
+						} else {
+							// TODO: Error Handling
+						}
+					} else if (next_char == '"') {
+						String newTok = "\"";
+						int j = i + 1;
+						char look_ahead = next_line.charAt(j);
+						while(isLetter(look_ahead) || isDigit(look_ahead) || look_ahead == ' ') {
+							newTok += look_ahead;
+							j++;
+							look_ahead = next_line.charAt(j);
+						}
+						if (look_ahead == '"') {
+							Token newToken = new Token(newTok, filename, lineNum, TokenType.ID_KEYWORD);
+							outList.add(newToken);
+						} else {
+							// TODO: Error Handling
+						}
+					}
+
+
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return outList;
+	}
+
+	private static boolean isDigit(char c) {
+		return switch (c) {
+			case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> true;
+			default -> false;
+		};
+	}
+
+	private static boolean isLetter(char c) {
+		return switch (c) {
+			case 'A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F', 'f' -> true;
+			case 'G', 'g', 'H', 'h', 'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l' -> true;
+			case 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r' -> true;
+			case 'S', 's', 'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x' -> true;
+			case 'Y', 'y', 'Z', 'z' -> true;
+			default -> false;
+		};
 	}
 }
