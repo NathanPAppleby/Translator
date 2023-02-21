6fe49a7b3f09ae759fe9fd03100d5942d05a1e13
djs3634 <djs3634@rit.edu>
2023-02-21 16:31:45 -0500

    Completed returnStmtNode

--- nodes/ReturnStmtNode.java
+++ nodes/ReturnStmtNode.java
@@ -6,26 +6,42 @@ import provided.Token;
 import java.util.ArrayList;
 
 public class ReturnStmtNode implements JottTree {
-    static ReturnStmtNode parseReturnStmtNode(ArrayList<Token> tokens) { return null; }
+    private final ExprNode exprNode;
+
+    public ReturnStmtNode(ExprNode exprNode) {
+        this.exprNode = exprNode;
+    }
+
+    static ReturnStmtNode parseReturnStmtNode(ArrayList<Token> tokens) throws Exception {
+        Token returnToken = tokens.remove(0);
+        if(returnToken.getToken().equals("return")){
+            ExprNode exprNode = ExprNode.parseExprNode(tokens);
+            EndStmtNode.parseEndStmtNode(tokens);
+            return new ReturnStmtNode(exprNode);
+        }
+        else{
+            throw new Exception();
+        }
+    }
 
     @Override
     public String convertToJott() {
-        return null;
+        return "return " + exprNode.convertToJott() + ";";
     }
 
     @Override
     public String convertToJava(String className) {
-        return null;
+        return "return " + exprNode.convertToJava(className) + ";";
     }
 
     @Override
     public String convertToC() {
-        return null;
+        return "return " + exprNode.convertToC() + ";";
     }
 
     @Override
     public String convertToPython() {
-        return null;
+        return "return " + exprNode.convertToPython();
     }
 
     @Override
