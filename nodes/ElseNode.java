b878392a7042594b0e662af6b42334852ee62957
Nate Appleby <npa1508@rit.edu>
2023-02-28 09:05:30 -0500

    Did parseElseNode

--- nodes/ElseNode.java
+++ nodes/ElseNode.java
@@ -2,11 +2,34 @@ package nodes;
 
 import provided.JottTree;
 import provided.Token;
+import provided.TokenType;
 
 import java.util.ArrayList;
 
 public class ElseNode implements JottTree {
-    static ElseNode parseElseNode(ArrayList<Token> tokens) { return null; }
+    private BodyNode bodyNode;
+
+    private ElseNode(BodyNode bodyNode){
+        this.bodyNode = bodyNode;
+    }
+
+    static ElseNode parseElseNode(ArrayList<Token> tokens) throws Exception {
+        if (tokens.get(0).getToken().equals("else")) {
+            tokens.remove(0);
+            if(tokens.remove(0).getTokenType().equals(TokenType.L_BRACE)) {
+                BodyNode bodyNode = BodyNode.parseBodyNode(tokens);
+                if (tokens.remove(0).getTokenType().equals(TokenType.R_BRACE)) {
+                    return new ElseNode(bodyNode);
+                } else {
+                    throw new Exception();
+                }
+            } else {
+                throw new Exception();
+            }
+        } else {
+            return null;
+        }
+    }
 
     @Override
     public String convertToJott() {
