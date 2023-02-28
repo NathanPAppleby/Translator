d5e8a0aa7982732c4ed8b58bdff2dccc042162cc
mjb7036 <mjb7036@rit.edu>
2023-02-28 15:30:23 -0500

    Did BodyNode

--- nodes/BodyNode.java
+++ nodes/BodyNode.java
@@ -3,10 +3,35 @@ package nodes;
 import provided.JottTree;
 import provided.Token;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class BodyNode implements JottTree {
-    static BodyNode parseBodyNode(ArrayList<Token> tokens) { return null; }
+    private final ArrayList<BodyStmtNode> bodyStmtNodes;
+    private final ReturnStmtNode returnStmtNode;
+    public BodyNode(ArrayList<BodyStmtNode> bodyStmtNodes, ReturnStmtNode returnStmtNode) {
+        this.bodyStmtNodes = bodyStmtNodes;
+        this.returnStmtNode = returnStmtNode;
+    }
+    static BodyNode parseBodyNode(ArrayList<Token> tokens) {
+        ArrayList<BodyStmtNode> bodyStmtNodes = new ArrayList<>();
+        ReturnStmtNode returnStmtNode = null;
+        bodyStmtNodes = getbodystmtnodes(tokens, bodyStmtNodes);
+        try {
+            returnStmtNode = ReturnStmtNode.parseReturnStmtNode(tokens);
+        }
+        catch (Exception ignored) {}
+        return new BodyNode(bodyStmtNodes, returnStmtNode);
+    }
+
+    static ArrayList<BodyStmtNode> getbodystmtnodes(ArrayList<Token> tokens, ArrayList<BodyStmtNode> bodyStmtNodes) {
+        try {
+            bodyStmtNodes.add(BodyStmtNode.parseBodyStmtNode(tokens));
+            getbodystmtnodes(tokens, bodyStmtNodes);
+        }
+        catch (Exception ignored) {}
+        return bodyStmtNodes;
+    }
 
     @Override
     public String convertToJott() {
