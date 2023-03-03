e061fc22dd2bec4b07c4017eafbc929bc4f830e0
cjb2272 <cjb2272@rit.edu>
2023-03-03 15:42:39 -0500

    Fixed exprNode and OperationNode logic, still need to look through a bit. one thing I am unsure of

--- nodes/ExprNode.java
+++ nodes/ExprNode.java
@@ -17,32 +17,34 @@ import java.util.ArrayList;
  */
 public interface ExprNode extends JottTree {
 
+    //LOOK AT FIRST TOKEN AND DETERMINE WHAT     I COULD BE,
+
     static ExprNode parseExprNode(ArrayList<Token> tokens) throws Exception {
-        //if next token is constant Node- need to cut this down with a simple ask if constant node somehow
-        if (tokens.get(0).getTokenType().equals(TokenType.STRING) ||
-                tokens.get(0).getTokenType().equals(TokenType.NUMBER) ||
-                tokens.get(0).getToken().equals("true") || tokens.get(0).getToken().equals("false"))
-        {
+        //if we have a <bool> or a <str_literal> (both of TokenType String)
+        //we can return that right away bc it will not
+        //directly exist as one of our 4 potential operation nodes
+        if (tokens.get(0).getTokenType().equals(TokenType.STRING)) {
             return ConstantNode.parseConstantNode(tokens);
         }
-        else if (tokens.size() > 1) { //this might be pointless, tokens passed in will always be greater
-            // if Function Call Node. MUST Check for func call before id because func call starts with id
-            if (tokens.get(0).getTokenType().equals(TokenType.ID_KEYWORD) &&
-                    tokens.get(1).getTokenType().equals(TokenType.L_BRACKET)) {
-                return FuncCallNode.parseFuncCallNode(tokens);
+        ExprNode expressionNode = null; //will be <id> or <num>
+        if ( tokens.get(0).getTokenType().equals(TokenType.NUMBER) ) {
+            expressionNode = ConstantNode.parseConstantNode(tokens);
+        } else if ( tokens.get(0).getTokenType().equals(TokenType.ID_KEYWORD) ) {
+            //MUST Check for func call before id because func call starts with id
+            if ( tokens.get(1).getTokenType().equals(TokenType.L_BRACKET) ) {
+                expressionNode = FuncCallNode.parseFuncCallNode(tokens);
+            } else {
+                expressionNode = IdNode.parseIdNode(tokens);
             }
-            //if Operation Node MUST also check for func call before id because Operation can begin with id
-            if (tokens.get(1).getTokenType().equals(TokenType.MATH_OP) ||
-                    tokens.get(1).getTokenType().equals(TokenType.REL_OP)) {
-                return OperationNode.parseOperationNode(tokens);
-            }
-        } else if (tokens.get(0).getTokenType().equals(TokenType.ID_KEYWORD)) { // if next token is id
-            return IdNode.parseIdNode(tokens);
-        } else {
-            throw new Exception();
         }
-        return null;
-        //LOOK AT FIRST TOKEN AND DETERMINE WHAT     I COULD BE, 
+        if (tokens.get(0).getTokenType().equals(TokenType.MATH_OP) ||
+                tokens.get(0).getTokenType().equals(TokenType.REL_OP)) {
+            OperatorNode op = OperatorNode.parseOperatorNode(tokens);
+            ExprNode right = ExprNode.parseExprNode(tokens);
+            return new OperationNode(expressionNode, op, right);
+        } else { // we have a number, but no op or rel op follows, so our expression is simply a number
+            return expressionNode;
+        }
     }
 
 }
--- nodes/OperationNode.java
+++ nodes/OperationNode.java
@@ -1,9 +1,5 @@
 package nodes;
 
-import provided.Token;
-
-import java.util.ArrayList;
-
 /**
  * A Type of ExprNode, OperationNode follows the format <expr> operator <expr>
  * which can break down into one of the following:
@@ -13,67 +9,59 @@ import java.util.ArrayList;
  *     <n_expr> <rel_op> <n_expr>  (ExprNode RelOpNode ExprNode)
  */
 public class OperationNode implements ExprNode {
-    //private final ExprNode left;
+    private final ExprNode left;
     private final OperatorNode middle;
     private final ExprNode right;
 
-    /*
+
     OperationNode(ExprNode leftNode, OperatorNode middleNode, ExprNode rightNode) {
         this.left = leftNode;
         this.middle = middleNode;
         this.right = rightNode;
     }
 
-     */
-
-    OperationNode(OperatorNode middleNode, ExprNode rightNode) {
-        this.middle = middleNode;
-        this.right = rightNode;
-    }
-
-    /**
-     * @param tokens the mutable array of all remaining tokens
-     * @return instance of Operation node which has 3 children
-     * @throws Exception .
-     */
+    /*
     static OperationNode parseOperationNode(ArrayList<Token> tokens) throws Exception {
-        //ExprNode leftExpr = ExprNode.parseExprNode(tokens);
+        ExprNode leftExpr = ExprNode.parseExprNode(tokens);
         OperatorNode operation = OperatorNode.parseOperatorNode(tokens);
         ExprNode rightExpr = ExprNode.parseExprNode(tokens);
-        return new OperationNode(operation, rightExpr);
-        //return new OperationNode(leftExpr, operation, rightExpr);
+        //return new OperationNode(operation, rightExpr);
+        return new OperationNode(leftExpr, operation, rightExpr);
     }
+
+     */
 
     @Override
     public String convertToJott() {
-        return this.middle.convertToJott() + this.right.convertToJott();
-        //return this.left.convertToJott() + this.middle.convertToJott() + this.right.convertToJott();
+        return this.left.convertToJott() +
+                this.middle.convertToJott() +
+                this.right.convertToJott();
     }
 
     @Override
     public String convertToJava(String className) {
-        //return this.left.convertToJava(className) + this.middle.convertToJava(className) +
-                //this.right.convertToJava(className);
-        return null;
+        return this.left.convertToJava(className) +
+                this.middle.convertToJava(className) +
+                this.right.convertToJava(className);
     }
 
     @Override
     public String convertToC() {
-        //return this.left.convertToC() + this.middle.convertToC() + this.right.convertToC();
-        return null;
+        return this.left.convertToC() +
+                this.middle.convertToC() +
+                this.right.convertToC();
     }
 
     @Override
     public String convertToPython() {
-        //return this.left.convertToPython() + this.middle.convertToPython() + this.right.convertToPython();
-        return null;
+        return this.left.convertToPython() +
+                this.middle.convertToPython() +
+                this.right.convertToPython();
     }
 
     @Override
     public boolean validateTree() {
         return false;
     }
-
-    //when we call operation node, we have already removed the constant at the beginning
 
 }
