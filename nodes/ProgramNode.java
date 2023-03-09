4d9b564c63e78f45621108e3dfec7d95e0c3f2f7
Kevin Penkowski <kwp5892@rit.edu>
2023-03-09 15:49:14 -0500

    Added line number and filename to index error

--- nodes/ProgramNode.java
+++ nodes/ProgramNode.java
@@ -12,13 +12,14 @@ public class ProgramNode implements JottTree {
     public ProgramNode(FunctionListNode functionListNode){this.functionListNode = functionListNode;}
 
     public static ProgramNode parseProgramNode(ArrayList<Token> tokens) throws Exception {
+        Token lastToken = tokens.get(tokens.size()-1);
         try {
             FunctionListNode functionListNode = FunctionListNode.parseFunctionListNode(tokens);
             return new ProgramNode(functionListNode);
         }
         catch (Exception e) {
             if (e.getClass().equals(IndexOutOfBoundsException.class)) {
-                System.err.println("General Tokens Error: \n\tPrematurely ran out of tokens in parser.\n");
+                System.err.println(String.format("General Tokens Error: \n\tPrematurely ran out of tokens in parser.\n\t%s:%d\n", lastToken.getFilename(), lastToken.getLineNum()));
             }
             else {
                 System.err.println(e.getLocalizedMessage() == null ? e.toString() : e.getLocalizedMessage());
