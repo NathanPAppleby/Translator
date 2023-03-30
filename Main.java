c3c3ca8edaa00aa5011738168b8d376bdb629d24
Nate Appleby <npa1508@rit.edu>
2023-03-30 16:51:36 -0400

    Updated main function with output functionality

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 
 
 import provided.*;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.io.File;
 
 public class Main {
 
@@ -14,6 +16,13 @@ public class Main {
             JottTree tree = JottParser.parse(tokenList);
 
             System.out.println(tree.convertToJott());
+
+            File outFile = new File(args[1]);
+            if(outFile.createNewFile()) {
+                FileWriter writer = new FileWriter(args[1]);
+                writer.write(tree.convertToJott());
+                writer.close();
+            }
         }
         catch(Exception e) {
 
