dcc1e0c426412e983e8dfceb80a504f8395d1f78
djs3634 <djs3634@rit.edu>
2023-03-30 16:58:44 -0400

    Added input checker for Main.java

--- Main.java
+++ Main.java
@@ -7,25 +7,29 @@ import java.io.File;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String jottFile = args[0];
+    public static void main(String[] args) throws Exception {
+        if(args.length==2) {
+            String jottFile = args[0];
 
-        ArrayList<Token> tokenList = JottTokenizer.tokenize(jottFile);
+            ArrayList<Token> tokenList = JottTokenizer.tokenize(jottFile);
 
-        try {
-            JottTree tree = JottParser.parse(tokenList);
+            try {
+                JottTree tree = JottParser.parse(tokenList);
 
-            System.out.println(tree.convertToJott());
+                System.out.println(tree.convertToJott());
 
-            File outFile = new File(args[1]);
-            if(outFile.createNewFile()) {
-                FileWriter writer = new FileWriter(args[1]);
-                writer.write(tree.convertToJott());
-                writer.close();
+                File outFile = new File(args[1]);
+                if (outFile.createNewFile()) {
+                    FileWriter writer = new FileWriter(args[1]);
+                    writer.write(tree.convertToJott());
+                    writer.close();
+                }
+            } catch (Exception e) {
+
             }
         }
-        catch(Exception e) {
-
+        else{
+            throw new Exception("Invalid input. Valid input:\njava Main.java {input file} {output file");
         }
 
     }
